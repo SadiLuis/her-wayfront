94922e78d5938ebaf1f44d4bb9a8d17034f0628f
Janina Roberto <janinasoledadroberto@gmail.com>
2022-03-16 14:40:36 -0300

    logo tentativo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
+import mujeres from './assets/mujeresunidas.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={mujeres} className="App-logo" alt="logo" />
+        <h1>HER WAY APP</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
