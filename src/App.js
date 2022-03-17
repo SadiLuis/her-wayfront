0213d332179a08df9ba750fec2cf86d1436a6e61
unknown <egil90@hotmail.com>
2022-03-17 15:32:50 -0600

    LandingPage con styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import Landing from './Components/Landing/Landing'
+import Slider from './Components/Landing/Slider'
+import Navbar from './Components/Landing/Navbar'
+import MujerPasajera from './Media/MujerPasajera.JPG'
+import MujerLibre from './Media/mujerLibre.JPG'
+import PulgarArriba from './Media/pulgarArriba.JPG'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      
-      </header>
+      <Navbar />
+      <Landing imageSrc={MujerPasajera} />
+      <Slider imageSrc={MujerLibre} title={'Sed ut perspiciatis unde omnis iste natus error sit voluptatem'} />
+      <Slider imageSrc={PulgarArriba} title={'Ut enim ad minima veniam, quis nostrum exercitationem'} flipped={true} />
     </div>
   );
 }
