764e936a7b1acf0393cd8554b0885e2f7d1d2882
polacomark <you@example.com>
2022-03-18 10:34:35 -0300

    para el admin

--- src/Reducer/index.js
+++ src/Reducer/index.js
@@ -1,7 +1,34 @@
 const initialState = {
-    user: [],
-    conductora: [],
+    conductoras: [],
+    allconductoras: [],
+    filters: [],
+    detail: []
 }
 
 export default function rootReducer(state = initialState, action) {
+switch(action.type){
+    case 'GET_ALL_CONDUCTORAS':
+        return{
+            ...state,
+            allconductoras: action.payload,
+            filters: action.payload
+        }
+        case 'GET_CONDUNCTORAS_NAME':
+            return {
+                ...state,
+                filters: action.payload,
+                allconductoras: action.payload
+            }
+         case 'CONDUCTORAS_DETAIL':
+             return{
+                 ...state,
+                 allconductoras: action.payload,
+                 detail: action.payload
+             } 
+         case 'POST_CONDUCTORAS':
+             return{
+                 ...state,
+             }      
+}
+
 }
--- src/actions/conductora.js
+++ src/actions/conductora.js
@@ -1,16 +1,58 @@
 import axios from "axios";
-import {PEDIR_CONDUCTORA} from "./index"
 
+export function getAllConductoras(){
+    return async function(dispatch){
+        try{
+            const conductoras = await axios.get('http://localhost:3001/conductoras')
+            return dispatch({
+                type:'GET_ALL_CONDUCTORAS',
+                payload: conductoras.data
+            })
 
-export const pedirConductora =()=> async dispatch=> {
-    try {
-        const respuesta= await axios('aca va la url')
-        const data=await respuesta.json();
-        return dispatch({
-            type:PEDIR_CONDUCTORA,
-            payload:data
-        })
-    } catch (error) {
-        return console.log("No se encontraron conductoras")
+        }catch(err){
+            console.log(err)
+        }
     }
+}
+export function getConductorasName(name){
+    try{
+        return async function (dispatch){
+            const json = await axios.get('http://localhost:3001/conductoras?name=' + name);
+            return dispatch({
+                type:'GET_CONDUNCTORAS_NAME',
+                payload: json.data
+            }
+            )
+        }
+    }catch(err){
+        console.log(err)
+    }
+}
+
+export function conductorasDetail(id){
+    try{
+        return async function (dispatch){
+            const detail = await axios.get('http://localhost:3001/conductoras/' + id);
+            return dispatch({
+                type: 'CONDUCTORAS_DETAIL',
+                payload: detail.data,
+            })
+        }
+    }catch(err){
+        console.log(err)
+    }  
+}
+
+export function postConductoras(payload){
+    try{
+        return async function (dispatch){
+            const create = await axios.get('http://localhost:3001/conductoras/register' + payload);
+            return dispatch({
+                type:'POST_CONDUCTORAS',
+                create,
+            })
+        }
+    }catch(err){
+        console.log(err)
+    }  
 }
