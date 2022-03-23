d8a99fb38ee3f0f2fc83b79e5107c31c18ff63ab
Janina Roberto <janinasoledadroberto@gmail.com>
2022-03-23 17:10:14 -0300

    eliminando cosas duplicadas

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import ResetPassword from './Components/ResetPassword';
 import PedirConductora from './Components/PedirConductora';
 import TarjetaConductora from './Screens/TarjetaConductora';
 import OrigenDestino from './Components/OrigenDestino/OrigenDestino';
-//  import VistaMap from './Screens/VistaMap.js'
+
 
 function App() {
 
@@ -44,7 +44,7 @@ function App() {
             <Route path='/conductora/register' element={<RegConductora />}/>
           <Route path="/pedirconductora" element={<PedirConductora/>} />
           <Route path="/tarjetaconductora" element={<TarjetaConductora/>} />
-          {/* <Route path="/mapa" element={<VistaMap />} />  */}
+         
           <Route path="origendestino" element={<OrigenDestino/>}/>
 
           </Routes>
