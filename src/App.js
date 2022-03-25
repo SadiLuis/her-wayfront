e536e23c6da2213054d2c6002b556efccbcb7da0
LoreLor <lorenadearmas01@gmail.com>
2022-03-25 12:38:23 -0300

    resolviendo conflictos map

--- src/App.js
+++ src/App.js
@@ -7,23 +7,20 @@ import Login from './Components/Login';
 import Registro from './Components/Registro';
 import PerfilConductora from './Components/Perfiles/PerfilConductora';
 import PerfilPasajera from './Components/Perfiles/PerfilPasajera';
+
 import RegConductora from './Components/RegConductora/RegConductora'
+
+
 import FiltrosConductoras from './Components/Pedidos/FiltrosConductoras';
+
 import ResetPassword from './Components/ResetPassword';
 import PedirConductora from './Components/PedirConductora';
 import TarjetaConductora from './Screens/TarjetaConductora';
-import LoginAdmin from './Components/Admin/LoginAdmin/LoginAdmin';
-import UsuariosAdmin from './Components/Admin/UsuariosAdmin/UsuariosAdmin'
-import DetalleConductora from './Components/Admin/DetalleConductora/DetalleConductora';
-import VerPasajerasAdmin from './Components/Admin/VerPasajerasAdmin/VerPasajerasAdmin';
-import VerificarConductora from './Components/Admin/VerificarUsuario/VerificarUsuario';
-import DarBajaConductora from './Components/Admin/DarBajaConductora/DarBajaConductora';
-import AltaAdmins from './Components/Admin/AltaAdmins/AltaAdmins';
-import OrigenDestino from './Components/OrigenDestino/OrigenDestino';
 import VistaMap from './Screens/VistaMap.jsx'
+
 import BotonPago from './Components/BotonPago/BotonPago';
 import LoginConductora from './Components/LoginConductora'
-import HomeConductora from './Screens/HomeConductora';
+
 
 function App() {
 
@@ -38,7 +35,7 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/registro" element={<Registro />} />
             <Route path="/home" element={<Home />} />
-            <Route exact path="/" element={<Landing />} />            
+            <Route exact path="/" element={<Landing />} /> 
             <Route path="/perfilConductora" element={<PerfilConductora />} />
             <Route path="/perfilPasajera" element={<PerfilPasajera />} />
             <Route path="/conductoras" element={<FiltrosConductoras />} />
@@ -47,21 +44,9 @@ function App() {
             <Route path='/conductora/register' element={<RegConductora />}/>
             <Route path="/pedirconductora" element={<PedirConductora/>} />
             <Route path="/tarjetaconductora" element={<TarjetaConductora/>} />
-            <Route path="/admin" element={<LoginAdmin/>}/>
-            <Route path="/admin/usuarios" element={<UsuariosAdmin/>}/>
-            <Route path="/admin/pasajeras" element={<VerPasajerasAdmin/>}/>
-            <Route path='/admin/:id' element={<DetalleConductora/>}/>
-            <Route path='/admin/verificar/:id' element={<VerificarConductora/>}/>
-            <Route path='/admin/darBajaConductora/:id' element={<DarBajaConductora/>}/>
-            <Route path='/admin/altaAdmins' element={<AltaAdmins/>}/>         
-            <Route path="origendestino" element={<OrigenDestino/>}/>
             <Route path="/mapa" element={<VistaMap />} />
             <Route path="/botonpago" element={<BotonPago />} />
-            <Route path="/homeConductora" element={<HomeConductora />} />
-
           </Routes>
-
-
         </BrowserRouter>
       </div>
     </div>
