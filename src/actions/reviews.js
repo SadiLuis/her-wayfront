5ec5fd06cab89be6d41bf000ddee7e1ca0c2296a
LoreLor <lorenadearmas01@gmail.com>
2022-03-26 14:08:18 -0300

    acomodando cambios que hizo git en action_review

--- src/actions/reviews.js
+++ src/actions/reviews.js
@@ -1,109 +1,35 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import style from './Reviews.module.css';
-import ReactStars from "react-rating-stars-component";
-import { postReview } from "../../actions/reviews";
-import Swal from 'sweetalert2';
+import {
+    CREAR_REVIEW,
+    OBTENER_REVIEW,
+    BORRAR_REVIEW,
+} from '../actions/reviews.js';
 
-const CreateReviews = (props) => {
-    const dispatch = useDispatch();
-
-    const [values, setValues] = useState({
-        idConductora: props.idConductora,
-        idPasajera: props.idPasajera,
-        conductora: "",
-        pasajera: "",
-        puntaje: "",
-        comentario: ""
-    })
-
-           
-
-    // cambio de comentarios
-    const handleOnChange = e => {
-        setValues({
-            ...values,
-            [e.target.name]: e.target.value
-        })
-        console.log('Values :>> ', values);
-    }
-
-    //cambio de rating o star o puntuacion
-    const ratingChanged = (newRating) => {
-        // console.log('newRating :>> ', newRating);
-        setValues({
-            ...values,
-            puntaje: newRating.toString()
-        })
-        console.log('Values :>> ', values);
-    };
-
-    const handleSubmit = (e) => {// funcion que enviara los datos de mi formulario 
-        e.preventDefault()
-        if (values.puntaje ==='' ||  values.comentario === '') {
-            Swal.fire({
-                icon: 'error',
-                title: 'Ups.. Falta Ingresar la puntuación de ★★★★★ o el Comentario ',
-                // text: 'o un comentario',
-                // footer: '<a href="">Why do I have this issue?</a>'
-            })
-        } else {
-            dispatch(postReview(props.idConductora, values))
-            setValues({
-                idConductora: props.idConductora,
-                idPasajera: props.idPasajera,
-                conductora: "",
-                pasajera: "",
-                puntaje: "",
-                comentario: ""
-            })
-            //window.location = `/conductora/:id`
-        }
-        
-    }
-
-
-    return (
-        <div>
-            <form className={style.form_review} onSubmit={handleSubmit}>
-                {/* ratings o stars */}
-                <div className={style.createStar}>
-                    <p>Puntua a tu Conductora</p>
-                    <ReactStars
-                    count={5}
-                    onChange={ratingChanged}
-                    size={28}
-                    // activeColor="#ffd700"
-                    activeColor="rgb(0, 72, 181)"
-                    />
-                </div>
-                {/* caja del comentario */}
-                <div className={style.caja}>
-                    <textarea
-                    className={style.description}
-                    title="maximo 120 caracteres"
-                    placeholder="Ingrese su Comentario"
-                    // rows="3"
-                    // cols="70"
-                    resize="none"
-                    value={values.comentario}
-                    onChange={handleOnChange}
-                    name="comentario"
-                    maxLength="120"                
-                    >
-                    </textarea>
-                    <button
-                    type="submit"
-                    className={style.boton}
-                    title="Click aqui para enviar comentario"
-                    >
-                    Enviar Comentario
-                    </button>
-                </div>
-                
-            </form>
-        </div>
-    )
+const initialState = {
+    crearReview :{},
+    obtenerReview:[],
+    borrarReview:{}
 }
-export default CreateReviews;
+
+export default function usersReducer(state = initialState, action){
+    switch (action.type) {
+        case CREAR_REVIEW:
+            return {
+                ...state,
+                crearReview: action.payload
+            }
+
+        case OBTENER_REVIEW:
+            return {
+                ...state,
+                obtenerReview: action.payload
+            }
+        case BORRAR_REVIEW:
+            return {
+                ...state,
+                ...state,
+                obtenerReview: state.obtenerReview.filter((e) => e.idReviews !== action.payload.idReviews)
+            }
+        default:
+            return state;
+        }
+    }
