7382229a2faec9fbfcb5de4b8a73e4e291dafd43
Emi <emibarberis26@gmail.com>
2022-03-28 16:20:13 -0300

    fix bug

--- src/Components/DetalleRecorrido/DetalleRecorrido.jsx
+++ src/Components/DetalleRecorrido/DetalleRecorrido.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch , useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom';
 import { getPerfilConductora } from '../../actions/conductora';
 import styles from './DetalleRecorrido.module.css'
@@ -7,19 +7,16 @@ import styles from './DetalleRecorrido.module.css'
 const DetalleRecorrido = ({idConductora}) => {
     const dispatch = useDispatch();
     const navigate = useNavigate()
-    useEffect(()=>{
-        dispatch(getPerfilConductora())
-    })
-
+   
     const cancelarViaje= () => {
         // dispatch(cancelar(idViaje));
-        // navigate('./')
+         navigate('/mapa')
     }
   return (
     <div className={styles.card__perfil}>
         <img src='' alt='Foto perfil' className={styles.img__perfil}/>
         <section className={styles.data__perfil}>
-        <p>Nombre Conductor</p>
+        <p>Nombre Conductora</p>
         <p>Auto</p>
         <p>Modelo de auto</p>
 
