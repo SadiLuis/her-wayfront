874329b3b190b1a6bf08fd19313680a2eaed5a55
Bernardo Gonzalez <60318571+bernigonzalez@users.noreply.github.com>
2022-04-04 14:57:36 -0300

    estilos componente viajeConductora

--- src/Components/Perfiles/PerfilPasajera.jsx
+++ src/Components/Perfiles/PerfilPasajera.jsx
@@ -4,13 +4,11 @@ import Image from '../../Media/placeholder.png'
 import Navbar from '../Landing/Navbar'
 import './PerfilPasajera.css'
 import { useDropzone } from "react-dropzone";
-import { Link,useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import {useSelector , useDispatch} from 'react-redux'
-import {login , logout} from '../../actions/Usuarios'
+import {getPasajeras , logout} from '../../actions/Usuarios'
 import {Loader} from '../Loader/Loader'
 import {saveImages} from '../../Helpers/saveImage'
-import {updateFoto} from '../../actions/Usuarios'
-
 export default function PerfilPasajera() {
     const pasajera = useSelector(state => state.LoginRegisReducer.pasajera)
     const [imagen , setImagen] = useState('')
@@ -18,70 +16,69 @@ export default function PerfilPasajera() {
     const dispatch = useDispatch()
     console.log(pasajera)
     useEffect(()=>{
-       dispatch(login())
+       dispatch(getPasajeras())
     },[dispatch], imagen)
- 
+
+  const handleButton = () => {
+      dispatch(logout())
+      navigate('/home')
+  }
+  const onDrop = useCallback(async(oFile) => {
+    
       console.log(oFile)
      const urlImage = await saveImages(oFile[0]);
       console.log(urlImage)
       setImagen(urlImage)
-      await updateFoto(urlImage , pasajera[0].id)
+    
 }, []);
 
-
-// const { getRootProps, getInputProps} = useDropzone({
-//     accept: "image/jpeg",
-//     noKeyboard: true,
-//     multiple: false,
-//     onDrop
-// });
+const { getRootProps, getInputProps} = useDropzone({
+    accept: "image/jpeg",
+    noKeyboard: true,
+    multiple: false,
+    onDrop
+});
   
-     return (
-        <><div className='perfilCard'>
-            {pasajera.length ?
-                (<div className='upperContainer'>
-
-                    <div className='imageContainer'>
-                        <img src={pasajera[0].fotoPerfil ? pasajera[0].fotoPerfil : imagen ? imagen : Image}
-                            alt='profile pic'
-                            height='100%'
-                            width='100px' />
-                        {/*  <h1 className='usuario'> Usuario: lolo </h1>
-             <span className='lowerContainer'>
-                 <h2> Nombre: Lorena </h2>
-                 <h4> E-mail: lorena@gmail.com </h4>
-                 <h4> País: Arg </h4>
-                 <h4> Teléfono: 55599966 </h4>
-                 <h4> Provincia: Form </h4>
-                 <h4> Localidad: Fsa </h4> */}
-                        {/* </div> */}
+    return (
+         
+        
+        <div className='perfilCard'>
+         {pasajera.length  ?
+         ( <div className='upperContainer'>
+                {/* <span className='editIcon'>Editar Perfíl</span> */}
+                <div className='imageContainer'>
+                    <img src={pasajera[0].fotoPerfil ? pasajera[0].fotoPerfil : imagen ? imagen : Image}
+                        alt='profile pic'
+                        height='100%'
+                        width='100px' />
+                   {/*  <h1 className='usuario'> Usuario: lolo </h1>
+                    <span className='lowerContainer'>
+                        <h2> Nombre: Lorena </h2>
+                        <h4> E-mail: lorena@gmail.com </h4>
+                        <h4> País: Arg </h4>
+                        <h4> Teléfono: 55599966 </h4>
+                        <h4> Provincia: Form </h4>
+                        <h4> Localidad: Fsa </h4> */}
+                    {/* </div> */}
                         <div style={{ cursor: 'pointer' }}>
-                            <span className='lowerContainer'>
-                                <h1> Usuario: {pasajera[0].usuario} </h1>
-                                <h2> Nombre: {pasajera[0].nombre} </h2>
-                                <h4> E-mail: {pasajera[0].email} </h4>
-                                <h4> País: {pasajera[0].pais} </h4>
-                                <h4> Teléfono: {pasajera[0].telefono} </h4>
-                                <h4> Provincia: {pasajera[0].provincia} </h4>
-                                <h4> Ciudad: {pasajera[0].localidad} </h4>
-                                <h4>Dirección: {pasajera[0].direccion}</h4>
-                            </span>
-                            
-                        </div>
+                         <span className='lowerContainer'>
+                         <h1> Usuario: {pasajera[0].usuario} </h1> 
+                        <h2> Nombre: {pasajera[0].nombre} </h2>
+                        <h4> E-mail: {pasajera[0].email} </h4>
+                        <h4> País: {pasajera[0].pais} </h4>
+                        <h4> Teléfono: {pasajera[0].telefono} </h4>
+                        <h4> Provincia: {pasajera[0].provincia} </h4>
+                        <h4> Ciudad: {pasajera[0].localidad} </h4> 
+                    </span>
                     </div>
-                       
-                    <Link to='/editPasajera'>
-                        Editar
-                    </Link>
-                    <button {...getRootProps()} primary>Actualizar foto </button>
-                    <input {...getInputProps()} />
-                    <button className='logoutP' onClick={handleButton}> Salir </button>
-                    
-                    </div>
+                </div>
+                <button className='logoutP' onClick={handleButton}> Log out </button>
+                <button {...getRootProps()} primary>Actualizar foto </button>
+                <input {...getInputProps()}/>
                 
-                ) : <Loader />}
-        </div>
-                </>
+            </div>
+        ) : <Loader/>
+          }
+         </div>
     )
 }
-
--- src/Components/viajes/ViajeConductora.jsx
+++ src/Components/viajes/ViajeConductora.jsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Loader } from '../Loader/Loader';
 import Swal from "sweetalert2";
 import CardPasajera from './CardPasajera/CardPasajera';
+import Navbar from '../Landing/Navbar';
 
 function ViajeConductora() {
 
@@ -14,40 +15,64 @@ function ViajeConductora() {
 
     let conductora = useSelector((state) => state.perfilConductoraReducer.perfilConductora)
     let viajeRequerido = useSelector((state) => state.viajesReducer.viajeRequeridoConductora)
-    console.log(viajeRequerido)
+    
 
     useEffect(() => {
         dispatch(getPerfilConductora(idConductora))
+        dispatch(getViajeRequerido(idConductora))
     }, [])
-
-    // useEffect(() => {
-    //     dispatch(getViajeRequerido(idConductora))
-    // }, [])
 
     
     useEffect(() => {
         const interval = setInterval(() => {
             dispatch(getViajeRequerido(idConductora));
-        }, 6000);
+        }, 10000);
         return () => clearInterval(interval);
       }, []);
+
+    //   useEffect(() => {
+    //     console.log("se dispara useEffect getViajeRequerido")
+    //     dispatch(getViajeRequerido(idConductora));
+        
+    //   }, [viajeRequerido]);
 
   
   
   return (
       <div>
-            <div>ViajeConductora</div>
-            <div>Viajes de {conductora.nombre}</div>
-            {typeof(viajeRequerido) === 'object' ? 
-            <CardPasajera 
-                nombrePasajera={viajeRequerido[0]?.nombrePasajera}
-                precio={viajeRequerido[0]?.precio}
-                distancia={viajeRequerido[0]?.distanciaMetros}
-                origen={viajeRequerido[0]?.direcOrigen }
-                destino={viajeRequerido[0]?.direcDestino }
-                idViaje={viajeRequerido[1]?.idViaje }
+            <Navbar />
+            <br />
+            <br />
+            <br />
+            {typeof(viajeRequerido) === 'object' ?
+            
+            <div class='container d-flex align-items-center justify-content-center vh-100'>
+                 <div class='row'> 
+                        {/* Columna principal izquierda */}
+                        <div class="col-12 col-xl-6 ">
+                                    <CardPasajera 
+                                        fotoPerfil={"https://www.preferente.com/wp-content/uploads/2019/10/jovenmalaga_opt.jpg"}
+                                        nombrePasajera={viajeRequerido[0]?.nombrePasajera}
+                                        precio={viajeRequerido[0]?.precio}
+                                        distancia={viajeRequerido[0]?.distanciaMetros}
+                                        origen={viajeRequerido[0]?.direcOrigen }
+                                        destino={viajeRequerido[0]?.direcDestino }
+                                        idViaje={viajeRequerido[1]?.idViaje }
 
-            />
+                                    />
+                        </div>    
+
+                         {/* Columna principal derecha */}
+                        <div class="col-12 col-xl-6 ">
+                            <img src="https://i.stack.imgur.com/ddX9U.png" alt="imagen mapa" className='h-100 w-100'  />
+                        </div>   
+
+                 </div>
+
+            </div>
+
+
+
             : (<div>
                 <h1>Buscando un viaje para vos...</h1>
                 <Loader />
