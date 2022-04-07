b3d16a41ec87e7d2d5b5d5a6754418dcc7260f9c
Janina Roberto <janinasoledadroberto@gmail.com>
2022-04-07 08:14:55 -0300

    viajes por pasajera, saqué el cerrar sesión de logincon google, acomodé los cierres de regConductora

--- src/Components/Perfiles/PerfilPasajera.jsx
+++ src/Components/Perfiles/PerfilPasajera.jsx
@@ -89,6 +89,7 @@ const { getRootProps, getInputProps} = useDropzone({
         						<div class="contact">
         							<a className="btn btn-block btn-outline-danger" onClick={handleDelete}><i class="fa fa-envelope-o" ></i> Eliminar cuenta</a>
         							<a className="btn btn-block btn-outline-success" onClick={()=> navigate('/editPasajera')}><i class="fa fa-book" ></i> Editar perfil</a>
+									<button className='btn btn-block btn-outline-success' onClick={() => navigate("/historialViajes/" + pasajera[0].id)}> Ver historial de Viajes </button>
         						</div>
         					</div>
         				</div>
